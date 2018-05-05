c5635cab3ff6cf6e32a5cc3958d583dd2a6814c0
AlbertAY <ay_1013@outlook.com>
2018-05-05 21:49:47 +0800

    增加json工具

---
+++ src/Twad.DotNet.Utility/Json/Gzip.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+
+namespace Twad.DotNet.Utility.Json
+{
+    public class Gzip
+    {
+        #region 压缩流数据
+
+        /// <summary>
+        ///     压缩流数据
+        /// </summary>
+        /// <param name="sourceStream"></param>
+        /// <returns></returns>
+        public static byte[] EnCompress(Stream sourceStream)
+        {
+            var vMemory = new MemoryStream();
+
+            sourceStream.Seek(0, SeekOrigin.Begin);
+            vMemory.Seek(0, SeekOrigin.Begin);
+            try
+            {
+                using (var vZipStream = new GZipStream(vMemory, CompressionMode.Compress))
+                {
+                    var vFileByte = new byte[1024];
+                    var vRedLen = 0;
+                    do
+                    {
+                        vRedLen = sourceStream.Read(vFileByte, 0, vFileByte.Length);
+                        vZipStream.Write(vFileByte, 0, vRedLen);
+                    } while (vRedLen > 0);
+                }
+            }
+            finally
+            {
+                vMemory.Dispose();
+            }
+            return vMemory.ToArray();
+        }
+
+
+        /// <summary>
+        ///     压缩数据
+        /// </summary>
+        /// <param name="sourceStreambyte"></param>
+        /// <returns></returns>
+        public static byte[] EnCompress(byte[] sourceStreambyte)
+        {
+            using (var vMemory = new MemoryStream(sourceStreambyte))
+            {
+                return EnCompress(vMemory);
+            }
+        }
+
+        /// <summary>
+        ///     压缩数据
+        /// </summary>
+        /// <param name="sourceStreamStr"></param>
+        /// <returns></returns>
+        public static byte[] EnCompress(string sourceStreamStr)
+        {
+            byte[] sourceStreambyte = { };
+            try
+            {
+                sourceStreambyte = Encoding.Default.GetBytes(sourceStreamStr);
+                return EnCompress(sourceStreambyte);
+            }
+            catch (Exception)
+            {
+                sourceStreambyte = Encoding.Default.GetBytes("EnCompressError");
+                return EnCompress(sourceStreambyte);
+            }
+        }
+
+        #endregion
+
+        #region 解压数据流
+
+        /// <summary>
+        ///     解压数据流
+        /// </summary>
+        /// <param name="sourceStream"></param>
+        /// <returns></returns>
+        public static byte[] DeCompressByte(Stream sourceStream)
+        {
+            byte[] vUnZipByte = null;
+
+            using (var vMemory = new MemoryStream())
+            {
+                var vUnZipStream = new GZipStream(sourceStream, CompressionMode.Decompress);
+                try
+                {
+                    var vTempByte = new byte[1024];
+                    var vRedLen = 0;
+                    do
+                    {
+                        vRedLen = vUnZipStream.Read(vTempByte, 0, vTempByte.Length);
+                        vMemory.Write(vTempByte, 0, vRedLen);
+                    } while (vRedLen > 0);
+                    vUnZipStream.Close();
+                }
+                finally
+                {
+                    vUnZipStream.Dispose();
+                }
+                vUnZipByte = vMemory.ToArray();
+            }
+            return vUnZipByte;
+        }
+
+        /// <summary>
+        ///     解压数据流
+        /// </summary>
+        /// <param name="sourceByte"></param>
+        /// <returns></returns>
+        public static byte[] DeCompressByte(byte[] sourceByte)
+        {
+            using (var vMemory = new MemoryStream(sourceByte))
+            {
+                return DeCompressByte(vMemory);
+            }
+        }
+
+        /// <summary>
+        ///     解压数据流
+        /// </summary>
+        /// <param name="sourceByte"></param>
+        /// <returns></returns>
+        public static string DeCompressStr(byte[] sourceByte)
+        {
+            var rtStr = "DeCompressError";
+            try
+            {
+                var deCompress = DeCompressByte(sourceByte);
+                rtStr = Encoding.Default.GetString(deCompress);
+                return rtStr;
+            }
+            catch (Exception)
+            {
+                return rtStr;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ src/Twad.DotNet.Utility/Json/JsonNetHelper.cs
@@ -0,0 +1,181 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Twad.DotNet.Utility.Json
+{
+    public class JsonNetHelper
+    {
+        public static string EnJson(object result)
+        {
+            return EnJson(0, null, result, GetToken(2));
+        }
+
+        public static string EnJson(int reason, object result)
+        {
+            return EnJson(reason, null, result, GetToken(2));
+        }
+
+        public static string EnJson(int reason, string message, object result)
+        {
+            return EnJson(reason, message, result, GetToken(2));
+        }
+
+        public static string EnJson(int reason, string message, object result, string token)
+        {
+            var timeConverter = new IsoDateTimeConverter();
+
+            timeConverter.DateTimeFormat = "yyyy'-'MM'-'dd' 'HH':'mm':'ss";
+
+            var obj = new { Reason = reason, Message = message, Result = result, Token = token };
+
+            var jsonStr = JsonConvert.SerializeObject(obj, timeConverter);
+
+            return jsonStr;
+        }
+
+        public JToken DeJson(string json)
+        {
+            var jt = JsonConvert.DeserializeObject<JToken>(json);
+            return jt;
+        }
+
+        public static byte[] EnEncodingJson(object result)
+        {
+            return EnEncodingJson(0, null, result, GetToken(2));
+        }
+
+        public static byte[] EnEncodingJson(int reason, object result)
+        {
+            return EnEncodingJson(reason, null, result, GetToken(2));
+        }
+
+        public static byte[] EnEncodingJson(int reason, string message, object result)
+        {
+            return EnEncodingJson(reason, message, result, GetToken(2));
+        }
+
+        public static byte[] EnEncodingJson(int reason, string message, object result, string token)
+        {
+            var timeConverter = new IsoDateTimeConverter();
+
+            timeConverter.DateTimeFormat = "yyyy'-'MM'-'dd' 'HH':'mm':'ss";
+
+            var obj = new { Reason = reason, Message = message, Result = result, Token = token };
+
+            var jsonByte = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(obj, timeConverter));
+
+            return jsonByte;
+        }
+
+        public JToken DeEncodingJson(byte[] jsonByte)
+        {
+            var jsonStr = Encoding.UTF8.GetString(jsonByte);
+            var jt = JsonConvert.DeserializeObject<JToken>(jsonStr);
+            return jt;
+        }
+
+        public static byte[] EnGzipJson(object result)
+        {
+            return EnGzipJson(0, null, result, GetToken(2));
+        }
+
+        public static byte[] EnGzipJson(int reason, object result)
+        {
+            return EnGzipJson(reason, null, result, GetToken(2));
+        }
+
+        public static byte[] EnGzipJson(int reason, string message, object result)
+        {
+            return EnGzipJson(reason, message, result, GetToken(2));
+        }
+
+        public static byte[] EnGzipJson(int reason, string message, object result, string token)
+        {
+            var timeConverter = new IsoDateTimeConverter();
+
+            timeConverter.DateTimeFormat = "yyyy'-'MM'-'dd' 'HH':'mm':'ss";
+
+            var obj = new { Reason = reason, Message = message, Result = result, Token = token };
+
+            var jsonByte = Gzip.EnCompress(JsonConvert.SerializeObject(obj, timeConverter));
+
+            return jsonByte;
+        }
+
+        public JToken DeGzipJson(byte[] jsonByte)
+        {
+            var jsonStr = Gzip.DeCompressStr(jsonByte);
+            var jt = JsonConvert.DeserializeObject<JToken>(jsonStr);
+            return jt;
+        }
+
+        /// <summary>
+        ///     获取Token(即方法名)
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        private static string GetToken(int index)
+        {
+            try
+            {
+                var month = new StackTrace().GetFrame(index).GetMethod();
+
+                return month.ReflectedType.Name + "_" + month.Name;
+            }
+            catch (Exception)
+            {
+                return "Auto generate token error!";
+            }
+        }
+
+        #region Json序列化/反序列化
+
+        public static T JsonDeserialize<T>(string Json)
+        {
+            try
+            {
+                return JsonConvert.DeserializeObject<T>(Json);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public static string JsonSerialize<T>(T obj)
+        {
+            var settings = new JsonSerializerSettings();
+            settings.NullValueHandling = NullValueHandling.Ignore;
+
+            return JsonConvert.SerializeObject(obj, Formatting.None, settings);
+        }
+
+        public static List<T> JsonDeserializeList<T>(string Json)
+        {
+            var list = new List<T>();
+            try
+            {
+                return JsonConvert.DeserializeObject<List<T>>(Json);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public static string JsonSerializeList<T>(List<T> list)
+        {
+            var settings = new JsonSerializerSettings();
+            settings.NullValueHandling = NullValueHandling.Ignore;
+
+            return JsonConvert.SerializeObject(list, Formatting.None, settings);
+        }
+
+        #endregion Json序列化/反序列化
+    }
+}
